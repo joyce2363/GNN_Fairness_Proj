fixed equal signs

b_influence_computation_and_save2.py
@@ -64,8 +64,8 @@ def get_adj(_name):
         predict_attr="RECID"
     elif dataset_name == 'income':
         predict_attr = "income"
-    elif dataset_name = 'nba':
-        predict_attr == 'nba'
+    elif dataset_name == 'nba':
+        predict_attr = 'nba'
     elif dataset_name == 'pokec1' or dataset_name == 'pokec2':
         if dataset_name == 'pokec1':
             edges = np.load('../data/pokec_dataset/region_job_1_edges.npy')