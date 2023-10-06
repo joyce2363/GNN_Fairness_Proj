ec255786cc13815017fe6820a8ba3be3f83f60f2
joyce2363 <joyceguo2001@gmail.com>
2023-10-06 11:32:52 +0900

    added seed parameter

--- b_removing_and_testing.py
+++ b_removing_and_testing.py
@@ -338,19 +338,19 @@ def tst():
 
 
 if dataset_name == 'bail':
-    adj_ori, features_ori, labels_ori, idx_train_ori, idx_val_ori, idx_test_ori, sens_ori = load_bail('bail')
+    adj_ori, features_ori, labels_ori, idx_train_ori, idx_val_ori, idx_test_ori, sens_ori = load_bail('bail', seed = args.seed)
     norm_features_ori = feature_norm(features_ori)
     norm_features_ori[:, 0] = features_ori[:, 0]
     features_ori = feature_norm(features_ori)
 elif dataset_name == 'income':
-    adj_ori, features_ori, labels_ori, idx_train_ori, idx_val_ori, idx_test_ori, sens_ori = load_income('income')
+    adj_ori, features_ori, labels_ori, idx_train_ori, idx_val_ori, idx_test_ori, sens_ori = load_income('income', seed = args.seed)
     norm_features_ori = feature_norm(features_ori)
     norm_features_ori[:, 8] = features_ori[:, 8]
     features_ori = feature_norm(features_ori)
 elif dataset_name == 'pokec1':
-    adj_ori, features_ori, labels_ori, idx_train_ori, idx_val_ori, idx_test_ori, sens_ori = load_pokec_renewed(1)
+    adj_ori, features_ori, labels_ori, idx_train_ori, idx_val_ori, idx_test_ori, sens_ori = load_pokec_renewed(1, seed = args.seed)
 elif dataset_name == 'pokec2':
-    adj_ori, features_ori, labels_ori, idx_train_ori, idx_val_ori, idx_test_ori, sens_ori = load_pokec_renewed(2)
+    adj_ori, features_ori, labels_ori, idx_train_ori, idx_val_ori, idx_test_ori, sens_ori = load_pokec_renewed(2, seed = args.seed)
 
 edge_index_ori = convert.from_scipy_sparse_matrix(adj_ori)[0]
 
