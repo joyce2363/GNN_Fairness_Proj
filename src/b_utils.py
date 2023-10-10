fb0fb1d1b788a53b55316956aa72f63bafa0e4ac
joyce2363 <joyceguo2001@gmail.com>
2023-10-10 09:05:58 +0900

    utils

--- src/b_utils.py
+++ src/b_utils.py
@@ -217,10 +217,12 @@ def load_nba_parameters_fairGNN(dataset, seed, local, sens_attr = "country", pre
     idx_features_labels = pd.read_csv(os.path.join(path, "{}.csv".format(dataset)))
     header = list(idx_features_labels.columns)
     header.remove(predict_attr)
+    header.remove(sens_attr) #fairGNN
     header.remove("user_id")
     edges_unordered = np.genfromtxt(os.path.join(path,"{}_edges.txt".format(dataset)), dtype=int)
 
     features = sp.csr_matrix(idx_features_labels[header], dtype=np.float32)
+    print("idx_features_labels: ", idx_features_labels)
     labels = idx_features_labels[predict_attr].values
 
     idx = np.array(idx_features_labels["user_id"], dtype=int)
@@ -239,74 +241,27 @@ def load_nba_parameters_fairGNN(dataset, seed, local, sens_attr = "country", pre
 
     import random
     random.seed(20)
-    label_idx = np.where(labels>=0)[0]
-    random.shuffle(label_idx)
-    
-    idx_train = label_idx[:min(int(0.5 * len(label_idx)),label_number)]
-    idx_val = label_idx[int(0.5 * len(label_idx)):int(0.75 * len(label_idx))]
-    if test_idx:
-        idx_test = label_idx[label_number:]
-        idx_val = idx_test
-    else:
-        idx_test = label_idx[int(0.75 * len(label_idx)):]
+    print('labels :', labels)
+    # label_idx = np.where(labels>=0)[0]
+    label_idx_0 = np.where(labels == 0)[0]
+    label_idx_1 = np.where(labels == 1)[0]
 
+    random.shuffle(label_idx_0)
+    random.shuffle(label_idx_1)
+    print('label_idx_0 : ', label_idx_0)
+    print('label_idx_1 : ', label_idx_1)
+    # random.shuffle(label_idx)
+    idx_train = np.append(label_idx_0[:min(int(0.5 * len(label_idx_0)), label_number // 2)],
+                          label_idx_1[:min(int(0.5 * len(label_idx_1)), label_number // 2)])
+    idx_val = np.append(label_idx_0[int(0.5 * len(label_idx_0)):int(0.75 * len(label_idx_0))],
+                        label_idx_1[int(0.5 * len(label_idx_1)):int(0.75 * len(label_idx_1))])
+    idx_test = np.append(label_idx_0[int(0.75 * len(label_idx_0)):], label_idx_1[int(0.75 * len(label_idx_1)):])
 
-
-
-    sens = idx_features_labels[sens_attr].values
-
-    sens_idx = set(np.where(sens >= 0)[0])
-    idx_test = np.asarray(list(sens_idx & set(idx_test)))
+    sens = idx_features_labels[sens_attr].values.astype(int)
     sens = torch.FloatTensor(sens)
-    idx_sens_train = list(sens_idx - set(idx_val) - set(idx_test))
-    random.seed(seed)
-    random.shuffle(idx_sens_train)
-    idx_sens_train = torch.LongTensor(idx_sens_train[:sens_number])
-
-
     idx_train = torch.LongTensor(idx_train)
     idx_val = torch.LongTensor(idx_val)
     idx_test = torch.LongTensor(idx_test)
-    # print('Loading {} dataset from {}'.format(dataset,path))
-
-    # idx_features_labels = pd.read_csv(os.path.join(path,"{}.csv".format(dataset)))
-    # header = list(idx_features_labels.columns)
-    # header.remove("user_id")
-
-    # # header.remove(sens_attr)
-    # header.remove(predict_attr)
-    # print('len(header) :', len(header)) #95
-
-    # features = sp.csr_matrix(idx_features_labels[header], dtype=np.float32)
-    # labels = idx_features_labels[predict_attr].values
-    
-
-    # # build graph
-    # idx = np.array(idx_features_labels["user_id"], dtype=int)
-    # idx_map = {j: i for i, j in enumerate(idx)}
-    # edges_unordered = np.genfromtxt(os.path.join(path,"{}_edges.txt".format(dataset)), dtype=int)
-
-    # edges = np.array(list(map(idx_map.get, edges_unordered.flatten())),
-    #                  dtype=int).reshape(edges_unordered.shape)
-    # print('edges: ', edges)
-    # adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
-    #                     shape=(labels.shape[0], labels.shape[0]),
-    #                     dtype=np.float32)
-    # # build symmetric adjacency matrix
-    # adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
-
-    # # features = normalize(features)
-    # adj = adj + sp.eye(adj.shape[0])
-
-    # features = torch.FloatTensor(np.array(features.todense()))
-    # labels = torch.LongTensor(labels)
-    # # adj = sparse_mx_to_torch_sparse_tensor(adj)
-
-    # import random
-    # random.seed(seed)
-    # label_idx = np.where(labels>=0)[0]
-    # random.shuffle(label_idx)
-
     # idx_train = label_idx[:min(int(0.5 * len(label_idx)),label_number)]
     # idx_val = label_idx[int(0.5 * len(label_idx)):int(0.75 * len(label_idx))]
     # if test_idx:
@@ -333,9 +288,6 @@ def load_nba_parameters_fairGNN(dataset, seed, local, sens_attr = "country", pre
     # idx_val = torch.LongTensor(idx_val)
     # idx_test = torch.LongTensor(idx_test)
     
-
-    # random.shuffle(sens_idx)
-# try sens or sens.to(torch.device('cuda'))
     return adj, features, labels, idx_train, idx_val, idx_test, sens.to(torch.device('cuda'))
 
 def load_bail(dataset, seed, sens_attr="WHITE", predict_attr="RECID", path="dataset/bail/", label_number=1000):
